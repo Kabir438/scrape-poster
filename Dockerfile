4ffc952c07fec9030ec5f6cdc5d8f400341c1efd
Kabir Chawla <developer.project@doonschool.com>
2024-04-18 13:34:59 +0530

    sudo

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true \
 WORKDIR /usr/src/app
 
 COPY package*.json ./
-RUN npm ci
+RUN npm i -y sudo
 COPY . .
 CMD [ "node", "index.js" ]
--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.get("/scrape", (req, res) => {
 app.get("/", async (req, res) => {
   const language = req.query.language;
   const promoter = req.query.promoter;
-
+ 
   // console.log(req)
   return await scrapeLogic(res, language, promoter);
 });
-
+ 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
