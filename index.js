703141b9c8a9ac91ae1699e6d7e1cb24e1953ce9
Kabir Chawla <developer.project@doonschool.com>
2024-04-20 01:52:24 +0530

    cors

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const app = express();
 const PORT = process.env.PORT || 4001;
 
 app.get("/scrape", (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   scrapeLogic(res); 
 });
 
 app.get("/", async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   const language = req.query.language;
   const promoter = req.query.promoter;
   const download = !!req.query.download;
